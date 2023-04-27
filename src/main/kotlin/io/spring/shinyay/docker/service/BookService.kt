046d5c2bb73f678c04b5e6b85bd91eada36e5182
shinyay <shinya.com@gmail.com>
2023-04-27 12:04:33 +0900

    Add demo data with CommandLineRunner

--- src/main/kotlin/io/spring/shinyay/docker/service/BookService.kt
+++ src/main/kotlin/io/spring/shinyay/docker/service/BookService.kt
@@ -2,10 +2,17 @@ package io.spring.shinyay.docker.service
 
 import io.spring.shinyay.docker.entity.Book
 import io.spring.shinyay.docker.repository.BookRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(val repository: BookRepository) {
+
+    @Bean
+    fun initialSetUp(repository: BookRepository) = CommandLineRunner {
+        repository.save(Book("Spring Boot 3.1 RC1", "shinyay"))
+    }
 
     fun findAllBooks(): MutableList<Book> = repository.findAll()
 }
