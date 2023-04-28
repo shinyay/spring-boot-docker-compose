cd451f8c63992875ed611f80c04d97269900711f
shinyay <shinya.com@gmail.com>
2023-04-28 09:09:42 +0900

    Add a post function for  persistance data

--- src/main/kotlin/io/spring/shinyay/docker/controller/BookController.kt
+++ src/main/kotlin/io/spring/shinyay/docker/controller/BookController.kt
@@ -1,7 +1,10 @@
 package io.spring.shinyay.docker.controller
 
+import io.spring.shinyay.docker.entity.Book
 import io.spring.shinyay.docker.service.BookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,4 +15,6 @@ class BookController(val service: BookService) {
     @GetMapping
     fun findAllBooks() = service.findAllBooks()
 
+    @PostMapping
+    fun registerBook(@RequestBody book: Book) = service.registerBook(book)
 }
